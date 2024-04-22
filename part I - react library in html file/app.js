525b47b804ee86756fa3ec4677684e0023a0a9d6
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-04-22 20:05:32 +0530

    Update app.js

--- part I - react library in html file/app.js
+++ part I - react library in html file/app.js
@@ -1,1 +1,13 @@
+class react_Container extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello! Welcome to Kalvium   
+                <div>This is babel</div>
+            </div>
+        );
+    }  
+}
 
+const container = document.getElementById('react_Container');
+ReactDOM.render(<react_Container/>, container);
